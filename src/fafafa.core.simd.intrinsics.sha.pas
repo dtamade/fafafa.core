53adfe42bd37c49f03f6f4bb268fc1f58d248f5c
dtama <dtama@fafafa.core>
2026-04-29 04:19:41 +0800

    feat(simd): implement experimental sha intrinsics semantics

--- src/fafafa.core.simd.intrinsics.sha.pas
+++ src/fafafa.core.simd.intrinsics.sha.pas
@@ -10,9 +10,9 @@ uses
 
 {
   Experimental status:
-  - This unit intentionally does NOT provide hardware-accurate SHA extension semantics.
   - By default, public APIs raise ENotSupportedException to avoid silent misuse.
-  - Define FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS to opt-in placeholder behavior.
+  - Define FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS to opt in scalar reference semantics.
+  - The opt-in path matches x86 SHA extension lane semantics but is not a tuned backend.
 }
 
 // SHA-1 intrinsics
@@ -35,7 +35,7 @@ procedure EnsureExperimentalIntrinsicsEnabled(const aFunctionName: string); inli
 begin
   {$IFNDEF FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS}
   raise ENotSupportedException.CreateFmt(
-    '%s is experimental placeholder semantics. Define FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS to opt in.',
+    '%s is experimental SHA semantics. Define FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS to opt in.',
     [aFunctionName]
   );
   {$ELSE}
@@ -44,65 +44,247 @@ begin
   {$ENDIF}
 end;
 
+function Add32(aLeft, aRight: DWord): DWord; inline;
+begin
+  Result := DWord(QWord(aLeft) + QWord(aRight));
+end;
+
+function Rol32(aValue: DWord; aCount: Byte): DWord; inline;
+var
+  LCount: Integer;
+begin
+  LCount := aCount and 31;
+  if LCount = 0 then
+    Exit(aValue);
+  Result := DWord((QWord(aValue) shl LCount) or (QWord(aValue) shr (32 - LCount)));
+end;
+
+function Ror32(aValue: DWord; aCount: Byte): DWord; inline;
+var
+  LCount: Integer;
+begin
+  LCount := aCount and 31;
+  if LCount = 0 then
+    Exit(aValue);
+  Result := DWord((QWord(aValue) shr LCount) or (QWord(aValue) shl (32 - LCount)));
+end;
+
+function Sha1Choose(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := (aB and aC) xor ((not aB) and aD);
+end;
+
+function Sha1Parity(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := aB xor aC xor aD;
+end;
+
+function Sha1Majority(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := (aB and aC) xor (aB and aD) xor (aC and aD);
+end;
+
+function Sha1RoundLogic(aFunc: Byte; aB, aC, aD: DWord): DWord; inline;
+begin
+  case (aFunc and 3) of
+    0:
+      Result := Sha1Choose(aB, aC, aD);
+    1, 3:
+      Result := Sha1Parity(aB, aC, aD);
+  else
+    Result := Sha1Majority(aB, aC, aD);
+  end;
+end;
+
+function Sha1RoundConstant(aFunc: Byte): DWord; inline;
+begin
+  case (aFunc and 3) of
+    0:
+      Result := DWord($5A827999);
+    1:
+      Result := DWord($6ED9EBA1);
+    2:
+      Result := DWord($8F1BBCDC);
+  else
+    Result := DWord($CA62C1D6);
+  end;
+end;
+
+function Sha256Choose(aE, aF, aG: DWord): DWord; inline;
+begin
+  Result := (aE and aF) xor ((not aE) and aG);
+end;
+
+function Sha256Majority(aA, aB, aC: DWord): DWord; inline;
+begin
+  Result := (aA and aB) xor (aA and aC) xor (aB and aC);
+end;
+
+function Sha256SmallSigma0(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 7) xor Ror32(aValue, 18) xor (aValue shr 3);
+end;
+
+function Sha256SmallSigma1(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 17) xor Ror32(aValue, 19) xor (aValue shr 10);
+end;
+
+function Sha256BigSigma0(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 2) xor Ror32(aValue, 13) xor Ror32(aValue, 22);
+end;
+
+function Sha256BigSigma1(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 6) xor Ror32(aValue, 11) xor Ror32(aValue, 25);
+end;
+
 function sha_sha1msg1_epu32(const a, b: TM128): TM128;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha1msg1_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] xor b.m128i_u32[0];
-  Result.m128i_u32[1] := a.m128i_u32[1] xor b.m128i_u32[1];
-  Result.m128i_u32[2] := a.m128i_u32[2] xor b.m128i_u32[2];
-  Result.m128i_u32[3] := a.m128i_u32[3] xor b.m128i_u32[3];
+  Result.m128i_u32[0] := a.m128i_u32[0] xor b.m128i_u32[2];
+  Result.m128i_u32[1] := a.m128i_u32[1] xor b.m128i_u32[3];
+  Result.m128i_u32[2] := a.m128i_u32[2] xor a.m128i_u32[0];
+  Result.m128i_u32[3] := a.m128i_u32[3] xor a.m128i_u32[1];
 end;
 
 function sha_sha1msg2_epu32(const a, b: TM128): TM128;
+var
+  LT3: DWord;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha1msg2_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] + b.m128i_u32[0];
-  Result.m128i_u32[1] := a.m128i_u32[1] + b.m128i_u32[1];
-  Result.m128i_u32[2] := a.m128i_u32[2] + b.m128i_u32[2];
-  Result.m128i_u32[3] := a.m128i_u32[3] + b.m128i_u32[3];
+  LT3 := Rol32(a.m128i_u32[3] xor b.m128i_u32[2], 1);
+  Result.m128i_u32[0] := Rol32(LT3 xor a.m128i_u32[0], 1);
+  Result.m128i_u32[1] := Rol32(a.m128i_u32[1] xor b.m128i_u32[0], 1);
+  Result.m128i_u32[2] := Rol32(a.m128i_u32[2] xor b.m128i_u32[1], 1);
+  Result.m128i_u32[3] := LT3;
 end;
 
 function sha_sha1nexte_epu32(const a, b: TM128): TM128;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha1nexte_epu32');
-  Result := a;
-  Result.m128i_u32[0] := Result.m128i_u32[0] + b.m128i_u32[3];
+  Result := b;
+  Result.m128i_u32[3] := Add32(b.m128i_u32[3], Rol32(a.m128i_u32[3], 30));
 end;
 
 function sha_sha1rnds4_epu32(const a, b: TM128; func: Byte): TM128;
+var
+  LA: DWord;
+  LB: DWord;
+  LC: DWord;
+  LD: DWord;
+  LE: DWord;
+  LTemp: DWord;
+  LWords: array[0..3] of DWord;
+  LRound: Integer;
+  LRoundLogic: DWord;
+  LRoundConstant: DWord;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha1rnds4_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] + b.m128i_u32[0] + func;
-  Result.m128i_u32[1] := a.m128i_u32[1] + b.m128i_u32[1] + func;
-  Result.m128i_u32[2] := a.m128i_u32[2] + b.m128i_u32[2] + func;
-  Result.m128i_u32[3] := a.m128i_u32[3] + b.m128i_u32[3] + func;
+  LWords[0] := b.m128i_u32[3];
+  LWords[1] := b.m128i_u32[2];
+  LWords[2] := b.m128i_u32[1];
+  LWords[3] := b.m128i_u32[0];
+
+  LD := a.m128i_u32[0];
+  LC := a.m128i_u32[1];
+  LB := a.m128i_u32[2];
+  LA := a.m128i_u32[3];
+  LE := 0;
+  LRoundConstant := Sha1RoundConstant(func);
+
+  for LRound := 0 to 3 do
+  begin
+    LRoundLogic := Sha1RoundLogic(func, LB, LC, LD);
+    LTemp := Add32(Rol32(LA, 5), LRoundLogic);
+    if LRound = 0 then
+      LTemp := Add32(LTemp, LWords[LRound])
+    else
+      LTemp := Add32(LTemp, Add32(LE, LWords[LRound]));
+    LTemp := Add32(LTemp, LRoundConstant);
+
+    LE := LD;
+    LD := LC;
+    LC := Rol32(LB, 30);
+    LB := LA;
+    LA := LTemp;
+  end;
+
+  Result.m128i_u32[0] := LD;
+  Result.m128i_u32[1] := LC;
+  Result.m128i_u32[2] := LB;
+  Result.m128i_u32[3] := LA;
 end;
 
 function sha_sha256msg1_epu32(const a, b: TM128): TM128;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha256msg1_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] + b.m128i_u32[0];
-  Result.m128i_u32[1] := a.m128i_u32[1] + b.m128i_u32[1];
-  Result.m128i_u32[2] := a.m128i_u32[2] + b.m128i_u32[2];
-  Result.m128i_u32[3] := a.m128i_u32[3] + b.m128i_u32[3];
+  Result.m128i_u32[0] := Add32(Sha256SmallSigma0(a.m128i_u32[1]), a.m128i_u32[0]);
+  Result.m128i_u32[1] := Add32(Sha256SmallSigma0(a.m128i_u32[2]), a.m128i_u32[1]);
+  Result.m128i_u32[2] := Add32(Sha256SmallSigma0(a.m128i_u32[3]), a.m128i_u32[2]);
+  Result.m128i_u32[3] := Add32(Sha256SmallSigma0(b.m128i_u32[0]), a.m128i_u32[3]);
 end;
 
 function sha_sha256msg2_epu32(const a, b: TM128): TM128;
+var
+  LO0: DWord;
+  LO1: DWord;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha256msg2_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] xor b.m128i_u32[0];
-  Result.m128i_u32[1] := a.m128i_u32[1] xor b.m128i_u32[1];
-  Result.m128i_u32[2] := a.m128i_u32[2] xor b.m128i_u32[2];
-  Result.m128i_u32[3] := a.m128i_u32[3] xor b.m128i_u32[3];
+  LO0 := Add32(Sha256SmallSigma1(b.m128i_u32[2]), a.m128i_u32[0]);
+  LO1 := Add32(Sha256SmallSigma1(b.m128i_u32[3]), a.m128i_u32[1]);
+  Result.m128i_u32[0] := LO0;
+  Result.m128i_u32[1] := LO1;
+  Result.m128i_u32[2] := Add32(Sha256SmallSigma1(LO0), a.m128i_u32[2]);
+  Result.m128i_u32[3] := Add32(Sha256SmallSigma1(LO1), a.m128i_u32[3]);
 end;
 
 function sha_sha256rnds2_epu32(const a, b, k: TM128): TM128;
+var
+  LA: DWord;
+  LB: DWord;
+  LC: DWord;
+  LD: DWord;
+  LE: DWord;
+  LF: DWord;
+  LG: DWord;
+  LH: DWord;
+  LT1: DWord;
+  LT2: DWord;
+  LRound: Integer;
 begin
   EnsureExperimentalIntrinsicsEnabled('sha_sha256rnds2_epu32');
-  Result.m128i_u32[0] := a.m128i_u32[0] + b.m128i_u32[0] + k.m128i_u32[0];
-  Result.m128i_u32[1] := a.m128i_u32[1] + b.m128i_u32[1] + k.m128i_u32[1];
-  Result.m128i_u32[2] := a.m128i_u32[2] + b.m128i_u32[2] + k.m128i_u32[2];
-  Result.m128i_u32[3] := a.m128i_u32[3] + b.m128i_u32[3] + k.m128i_u32[3];
+  LH := a.m128i_u32[0];
+  LG := a.m128i_u32[1];
+  LD := a.m128i_u32[2];
+  LC := a.m128i_u32[3];
+  LF := b.m128i_u32[0];
+  LE := b.m128i_u32[1];
+  LB := b.m128i_u32[2];
+  LA := b.m128i_u32[3];
+
+  for LRound := 0 to 1 do
+  begin
+    LT1 := Add32(LH, Sha256BigSigma1(LE));
+    LT1 := Add32(LT1, Sha256Choose(LE, LF, LG));
+    LT1 := Add32(LT1, k.m128i_u32[LRound]);
+    LT2 := Add32(Sha256BigSigma0(LA), Sha256Majority(LA, LB, LC));
+
+    LH := LG;
+    LG := LF;
+    LF := LE;
+    LE := Add32(LD, LT1);
+    LD := LC;
+    LC := LB;
+    LB := LA;
+    LA := Add32(LT1, LT2);
+  end;
+
+  Result.m128i_u32[0] := LF;
+  Result.m128i_u32[1] := LE;
+  Result.m128i_u32[2] := LB;
+  Result.m128i_u32[3] := LA;
 end;
 
 end.
--- tests/fafafa.core.simd.intrinsics.experimental/fafafa.core.simd.intrinsics.experimental.testcase.pas
+++ tests/fafafa.core.simd.intrinsics.experimental/fafafa.core.simd.intrinsics.experimental.testcase.pas
@@ -18,7 +18,7 @@ type
   published
     procedure Test_Default_AES_SHA_Rejects;
     procedure Test_Experimental_AES_Semantics;
-    procedure Test_Experimental_SHA_PlaceholderSemantics;
+    procedure Test_Experimental_SHA_Semantics;
   end;
 
 implementation
@@ -48,12 +48,271 @@ begin
     aValue.m128i_u8[LIndex] := aBytes[LIndex];
 end;
 
+procedure LoadM128Words(var aValue: TM128; const aWords: array of DWord);
+var
+  LIndex: Integer;
+begin
+  FillChar(aValue, SizeOf(aValue), 0);
+  for LIndex := 0 to High(aWords) do
+    aValue.m128i_u32[LIndex] := aWords[LIndex];
+end;
+
 procedure AssertM128BytesEqual(aTest: TTestCase; const aLabel: string; const aExpected, aActual: TM128);
 var
   LIndex: Integer;
 begin
   for LIndex := 0 to 15 do
     aTest.AssertEquals(aLabel + ' lane ' + IntToStr(LIndex), aExpected.m128i_u8[LIndex], aActual.m128i_u8[LIndex]);
+end;
+
+procedure AssertM128WordsEqual(aTest: TTestCase; const aLabel: string; const aExpected, aActual: TM128);
+var
+  LIndex: Integer;
+begin
+  for LIndex := 0 to 3 do
+    aTest.AssertEquals(aLabel + ' lane ' + IntToStr(LIndex), aExpected.m128i_u32[LIndex], aActual.m128i_u32[LIndex]);
+end;
+
+function Add32(aLeft, aRight: DWord): DWord; inline;
+begin
+  Result := DWord(QWord(aLeft) + QWord(aRight));
+end;
+
+function Rol32(aValue: DWord; aCount: Byte): DWord; inline;
+var
+  LCount: Integer;
+begin
+  LCount := aCount and 31;
+  if LCount = 0 then
+    Exit(aValue);
+  Result := DWord((QWord(aValue) shl LCount) or (QWord(aValue) shr (32 - LCount)));
+end;
+
+function Ror32(aValue: DWord; aCount: Byte): DWord; inline;
+var
+  LCount: Integer;
+begin
+  LCount := aCount and 31;
+  if LCount = 0 then
+    Exit(aValue);
+  Result := DWord((QWord(aValue) shr LCount) or (QWord(aValue) shl (32 - LCount)));
+end;
+
+function SHA1Choose(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := (aB and aC) xor ((not aB) and aD);
+end;
+
+function SHA1Parity(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := aB xor aC xor aD;
+end;
+
+function SHA1Majority(aB, aC, aD: DWord): DWord; inline;
+begin
+  Result := (aB and aC) xor (aB and aD) xor (aC and aD);
+end;
+
+function SHA256Choose(aE, aF, aG: DWord): DWord; inline;
+begin
+  Result := (aE and aF) xor ((not aE) and aG);
+end;
+
+function SHA256Majority(aA, aB, aC: DWord): DWord; inline;
+begin
+  Result := (aA and aB) xor (aA and aC) xor (aB and aC);
+end;
+
+function SHA256SmallSigma0(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 7) xor Ror32(aValue, 18) xor (aValue shr 3);
+end;
+
+function SHA256SmallSigma1(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 17) xor Ror32(aValue, 19) xor (aValue shr 10);
+end;
+
+function SHA256BigSigma0(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 2) xor Ror32(aValue, 13) xor Ror32(aValue, 22);
+end;
+
+function SHA256BigSigma1(aValue: DWord): DWord; inline;
+begin
+  Result := Ror32(aValue, 6) xor Ror32(aValue, 11) xor Ror32(aValue, 25);
+end;
+
+function ReferenceSha1Msg1(const aA, aB: TM128): TM128;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  Result.m128i_u32[0] := aA.m128i_u32[0] xor aB.m128i_u32[2];
+  Result.m128i_u32[1] := aA.m128i_u32[1] xor aB.m128i_u32[3];
+  Result.m128i_u32[2] := aA.m128i_u32[2] xor aA.m128i_u32[0];
+  Result.m128i_u32[3] := aA.m128i_u32[3] xor aA.m128i_u32[1];
+end;
+
+function ReferenceSha1Msg2(const aA, aB: TM128): TM128;
+var
+  LT3: DWord;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  LT3 := Rol32(aA.m128i_u32[3] xor aB.m128i_u32[2], 1);
+  Result.m128i_u32[0] := Rol32(LT3 xor aA.m128i_u32[0], 1);
+  Result.m128i_u32[1] := Rol32(aA.m128i_u32[1] xor aB.m128i_u32[0], 1);
+  Result.m128i_u32[2] := Rol32(aA.m128i_u32[2] xor aB.m128i_u32[1], 1);
+  Result.m128i_u32[3] := LT3;
+end;
+
+function ReferenceSha1Nexte(const aA, aB: TM128): TM128;
+begin
+  Result := aB;
+  Result.m128i_u32[3] := Add32(aB.m128i_u32[3], Rol32(aA.m128i_u32[3], 30));
+end;
+
+function ReferenceSha1RoundLogic(aFunc: Byte; aB, aC, aD: DWord): DWord; inline;
+begin
+  case (aFunc and 3) of
+    0:
+      Result := SHA1Choose(aB, aC, aD);
+    1, 3:
+      Result := SHA1Parity(aB, aC, aD);
+  else
+    Result := SHA1Majority(aB, aC, aD);
+  end;
+end;
+
+function ReferenceSha1RoundConstant(aFunc: Byte): DWord; inline;
+begin
+  case (aFunc and 3) of
+    0:
+      Result := DWord($5A827999);
+    1:
+      Result := DWord($6ED9EBA1);
+    2:
+      Result := DWord($8F1BBCDC);
+  else
+    Result := DWord($CA62C1D6);
+  end;
+end;
+
+function ReferenceSha1Rnds4(const aA, aB: TM128; aFunc: Byte): TM128;
+var
+  LA: DWord;
+  LB: DWord;
+  LC: DWord;
+  LD: DWord;
+  LE: DWord;
+  LTemp: DWord;
+  LRound: Integer;
+  LWords: array[0..3] of DWord;
+  LRoundLogic: DWord;
+  LRoundConstant: DWord;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  LWords[0] := aB.m128i_u32[3];
+  LWords[1] := aB.m128i_u32[2];
+  LWords[2] := aB.m128i_u32[1];
+  LWords[3] := aB.m128i_u32[0];
+
+  LD := aA.m128i_u32[0];
+  LC := aA.m128i_u32[1];
+  LB := aA.m128i_u32[2];
+  LA := aA.m128i_u32[3];
+  LE := 0;
+  LRoundConstant := ReferenceSha1RoundConstant(aFunc);
+
+  for LRound := 0 to 3 do
+  begin
+    LRoundLogic := ReferenceSha1RoundLogic(aFunc, LB, LC, LD);
+    LTemp := Add32(Rol32(LA, 5), LRoundLogic);
+    if LRound = 0 then
+      LTemp := Add32(LTemp, LWords[LRound])
+    else
+      LTemp := Add32(LTemp, Add32(LE, LWords[LRound]));
+    LTemp := Add32(LTemp, LRoundConstant);
+
+    LE := LD;
+    LD := LC;
+    LC := Rol32(LB, 30);
+    LB := LA;
+    LA := LTemp;
+  end;
+
+  Result.m128i_u32[0] := LD;
+  Result.m128i_u32[1] := LC;
+  Result.m128i_u32[2] := LB;
+  Result.m128i_u32[3] := LA;
+end;
+
+function ReferenceSha256Msg1(const aA, aB: TM128): TM128;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  Result.m128i_u32[0] := Add32(SHA256SmallSigma0(aA.m128i_u32[1]), aA.m128i_u32[0]);
+  Result.m128i_u32[1] := Add32(SHA256SmallSigma0(aA.m128i_u32[2]), aA.m128i_u32[1]);
+  Result.m128i_u32[2] := Add32(SHA256SmallSigma0(aA.m128i_u32[3]), aA.m128i_u32[2]);
+  Result.m128i_u32[3] := Add32(SHA256SmallSigma0(aB.m128i_u32[0]), aA.m128i_u32[3]);
+end;
+
+function ReferenceSha256Msg2(const aA, aB: TM128): TM128;
+var
+  LO0: DWord;
+  LO1: DWord;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  LO0 := Add32(SHA256SmallSigma1(aB.m128i_u32[2]), aA.m128i_u32[0]);
+  LO1 := Add32(SHA256SmallSigma1(aB.m128i_u32[3]), aA.m128i_u32[1]);
+  Result.m128i_u32[0] := LO0;
+  Result.m128i_u32[1] := LO1;
+  Result.m128i_u32[2] := Add32(SHA256SmallSigma1(LO0), aA.m128i_u32[2]);
+  Result.m128i_u32[3] := Add32(SHA256SmallSigma1(LO1), aA.m128i_u32[3]);
+end;
+
+function ReferenceSha256Rnds2(const aA, aB, aK: TM128): TM128;
+var
+  LA: DWord;
+  LB: DWord;
+  LC: DWord;
+  LD: DWord;
+  LE: DWord;
+  LF: DWord;
+  LG: DWord;
+  LH: DWord;
+  LT1: DWord;
+  LT2: DWord;
+  LRound: Integer;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  LH := aA.m128i_u32[0];
+  LG := aA.m128i_u32[1];
+  LD := aA.m128i_u32[2];
+  LC := aA.m128i_u32[3];
+  LF := aB.m128i_u32[0];
+  LE := aB.m128i_u32[1];
+  LB := aB.m128i_u32[2];
+  LA := aB.m128i_u32[3];
+
+  for LRound := 0 to 1 do
+  begin
+    LT1 := Add32(LH, SHA256BigSigma1(LE));
+    LT1 := Add32(LT1, SHA256Choose(LE, LF, LG));
+    LT1 := Add32(LT1, aK.m128i_u32[LRound]);
+    LT2 := Add32(SHA256BigSigma0(LA), SHA256Majority(LA, LB, LC));
+
+    LH := LG;
+    LG := LF;
+    LF := LE;
+    LE := Add32(LD, LT1);
+    LD := LC;
+    LC := LB;
+    LB := LA;
+    LA := Add32(LT1, LT2);
+  end;
+
+  Result.m128i_u32[0] := LF;
+  Result.m128i_u32[1] := LE;
+  Result.m128i_u32[2] := LB;
+  Result.m128i_u32[3] := LA;
 end;
 
 procedure TTestCase_SimdIntrinsicsExperimental.Test_Default_AES_SHA_Rejects;
@@ -158,77 +417,97 @@ begin
   AssertM128BytesEqual(Self, 'aes_aeskeygenassist_si128 rcon=36', LExpected, LActual);
 end;
 
-procedure TTestCase_SimdIntrinsicsExperimental.Test_Experimental_SHA_PlaceholderSemantics;
+procedure TTestCase_SimdIntrinsicsExperimental.Test_Experimental_SHA_Semantics;
 var
-  LA, LB, LC, LResult: TM128;
-  LIndex: Integer;
-  LExpectedWord: DWord;
+  LA: TM128;
+  LB: TM128;
+  LK: TM128;
+  LExpected: TM128;
+  LActual: TM128;
 begin
   {$IFNDEF FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS}
   Exit;
   {$ENDIF}
 
-  FillChar(LA, SizeOf(LA), 0);
-  FillChar(LB, SizeOf(LB), 0);
-  FillChar(LC, SizeOf(LC), 0);
-  LA.m128i_u32[0] := 1;   LA.m128i_u32[1] := 10;  LA.m128i_u32[2] := 100;  LA.m128i_u32[3] := 1000;
-  LB.m128i_u32[0] := 2;   LB.m128i_u32[1] := 20;  LB.m128i_u32[2] := 200;  LB.m128i_u32[3] := 2000;
-  LC.m128i_u32[0] := 3;   LC.m128i_u32[1] := 30;  LC.m128i_u32[2] := 300;  LC.m128i_u32[3] := 3000;
+  LoadM128Words(LA, [
+    DWord($01234567), DWord($89ABCDEF), DWord($0F1E2D3C), DWord($4B5A6978)
+  ]);
+  LoadM128Words(LB, [
+    DWord($11111111), DWord($22222222), DWord($33333333), DWord($44444444)
+  ]);
+  LoadM128Words(LK, [
+    DWord($DEADBEEF), DWord($01020304), DWord($A5A5A5A5), DWord($5A5A5A5A)
+  ]);
 
-  LResult := sha_sha1msg1_epu32(LA, LB);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] xor LB.m128i_u32[LIndex];
-    AssertEquals('sha_sha1msg1_epu32 xor lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($32107654), DWord($CDEF89AB), DWord($0E3D685B), DWord($C2F1A497)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1msg1_epu32', LExpected, ReferenceSha1Msg1(LA, LB));
+  LActual := sha_sha1msg1_epu32(LA, LB);
+  AssertM128WordsEqual(Self, 'sha_sha1msg1_epu32', LExpected, LActual);
 
-  LResult := sha_sha1msg2_epu32(LA, LB);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] + LB.m128i_u32[LIndex];
-    AssertEquals('sha_sha1msg2_epu32 add lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($E3E3E3E3), DWord($3175B9FD), DWord($5A781E3C), DWord($F0D2B496)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1msg2_epu32', LExpected, ReferenceSha1Msg2(LA, LB));
+  LActual := sha_sha1msg2_epu32(LA, LB);
+  AssertM128WordsEqual(Self, 'sha_sha1msg2_epu32', LExpected, LActual);
 
-  LResult := sha_sha1nexte_epu32(LA, LB);
-  AssertEquals('sha_sha1nexte_epu32 lane0 add b3',
-    LA.m128i_u32[0] + LB.m128i_u32[3], LResult.m128i_u32[0]);
-  for LIndex := 1 to 3 do
-    AssertEquals('sha_sha1nexte_epu32 keep lane ' + IntToStr(LIndex),
-      LA.m128i_u32[LIndex], LResult.m128i_u32[LIndex]);
+  LoadM128Words(LExpected, [
+    DWord($11111111), DWord($22222222), DWord($33333333), DWord($571ADEA2)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1nexte_epu32', LExpected, ReferenceSha1Nexte(LA, LB));
+  LActual := sha_sha1nexte_epu32(LA, LB);
+  AssertM128WordsEqual(Self, 'sha_sha1nexte_epu32', LExpected, LActual);
 
-  LResult := sha_sha1rnds4_epu32(LA, LB, 7);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] + LB.m128i_u32[LIndex] + 7;
-    AssertEquals('sha_sha1rnds4_epu32 add lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($44CFCE95), DWord($1EA8F2A9), DWord($6DA05997), DWord($643E23C4)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1rnds4_epu32 func=0', LExpected, ReferenceSha1Rnds4(LA, LB, 0));
+  LActual := sha_sha1rnds4_epu32(LA, LB, 0);
+  AssertM128WordsEqual(Self, 'sha_sha1rnds4_epu32 func=0', LExpected, LActual);
 
-  LResult := sha_sha256msg1_epu32(LA, LB);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] + LB.m128i_u32[LIndex];
-    AssertEquals('sha_sha256msg1_epu32 add lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($A9808128), DWord($C929CA19), DWord($669FFE49), DWord($F7A3843E)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1rnds4_epu32 func=1', LExpected, ReferenceSha1Rnds4(LA, LB, 1));
+  LActual := sha_sha1rnds4_epu32(LA, LB, 1);
+  AssertM128WordsEqual(Self, 'sha_sha1rnds4_epu32 func=1', LExpected, LActual);
 
-  LResult := sha_sha256msg2_epu32(LA, LB);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] xor LB.m128i_u32[LIndex];
-    AssertEquals('sha_sha256msg2_epu32 xor lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($11F61F66), DWord($72936C7B), DWord($88768604), DWord($C59BB53B)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1rnds4_epu32 func=2', LExpected, ReferenceSha1Rnds4(LA, LB, 2));
+  LActual := sha_sha1rnds4_epu32(LA, LB, 2);
+  AssertM128WordsEqual(Self, 'sha_sha1rnds4_epu32 func=2', LExpected, LActual);
 
-  LResult := sha_sha256rnds2_epu32(LA, LB, LC);
-  for LIndex := 0 to 3 do
-  begin
-    LExpectedWord := LA.m128i_u32[LIndex] + LB.m128i_u32[LIndex] + LC.m128i_u32[LIndex];
-    AssertEquals('sha_sha256rnds2_epu32 add lane ' + IntToStr(LIndex),
-      LExpectedWord, LResult.m128i_u32[LIndex]);
-  end;
+  LoadM128Words(LExpected, [
+    DWord($C062B6B5), DWord($3C52B14A), DWord($B02522CB), DWord($07DEA16F)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha1rnds4_epu32 func=3', LExpected, ReferenceSha1Rnds4(LA, LB, 3));
+  LActual := sha_sha1rnds4_epu32(LA, LB, 3);
+  AssertM128WordsEqual(Self, 'sha_sha1rnds4_epu32 func=3', LExpected, LActual);
+
+  LoadM128Words(LExpected, [
+    DWord($3E8111B3), DWord($7C5EC829), DWord($72C21867), DWord($AF9EADBC)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha256msg1_epu32', LExpected, ReferenceSha256Msg1(LA, LB));
+  LActual := sha_sha256msg1_epu32(LA, LB);
+  AssertM128WordsEqual(Self, 'sha_sha256msg1_epu32', LExpected, LActual);
+
+  LoadM128Words(LExpected, [
+    DWord($0116789A), DWord($346789AA), DWord($027C3273), DWord($8147AED5)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha256msg2_epu32', LExpected, ReferenceSha256Msg2(LA, LB));
+  LActual := sha_sha256msg2_epu32(LA, LB);
+  AssertM128WordsEqual(Self, 'sha_sha256msg2_epu32', LExpected, LActual);
+
+  LoadM128Words(LExpected, [
+    DWord($5656DD3C), DWord($56C84A10), DWord($ACAD3392), DWord($B30376E9)
+  ]);
+  AssertM128WordsEqual(Self, 'reference sha_sha256rnds2_epu32', LExpected, ReferenceSha256Rnds2(LA, LB, LK));
+  LActual := sha_sha256rnds2_epu32(LA, LB, LK);
+  AssertM128WordsEqual(Self, 'sha_sha256rnds2_epu32', LExpected, LActual);
 end;
 
 {$IFDEF FAFAFA_SIMD_EXPERIMENTAL_INTRINSICS}
